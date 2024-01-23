05da903a8cb3e365c61bd05941b1e2c39987071c
A <A@LAPTOP-QVOQ8SSU>
2024-01-23 12:12:22 -0500

    Add project files.

---
+++ Homework1/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <float.h>
+using namespace std; 
+
+int main() {
+	cout << "Enter 10 floating point numbers" << endl;
+	double num[10], minimum = DBL_MAX, maximum = 0.0, mean = 0.0, stddev = 0.0;
+	double sum = 0.0;
+	int count = 0;
+
+	for (int i = 0; i < 10; i++) {
+		cout << "Enter number " << count << ": ";
+		cin >> num[i];
+		count++;
+		sum = num[i] + sum;
+		if (num[i] < minimum) {
+			minimum = num[i];
+		}
+		else if (num[i] > maximum) {
+			maximum = num[i];
+		}
+	}
+
+	mean = sum / count;
+
+	for (int i = 0; i < 10; i++) {
+		stddev = stddev + ((num[i] - mean) * (num[i] - mean));
+	}
+	stddev = sqrt(stddev / count);
+
+	cout << "\nThe mean of numbers is: " << mean << endl;
+	cout << "The minimum of numbers is: " << minimum << endl;
+	cout << "The maximum of numbers is: " << maximum << endl;
+	cout << "The standard deviation of numbers is: " << stddev << endl;	
+
+}
+
